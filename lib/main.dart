b902b51c96f7acd48e304376465e98f02f7e057f
Pallavi Pradhan <pallavipradhan5578@gmail.com>
2024-12-06 18:59:37 +0530

    Notification on & off via using getx.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:ThemeChanges  (name: 'Theme',),debugShowCheckedModeBanner: false,
+      home:ExampleThree  (name: 'Theme',),debugShowCheckedModeBanner: false,
     );
   }
 }
